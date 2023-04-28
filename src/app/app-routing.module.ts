e66235c893d31c74c85cbdf7e20b99e6364d8a38
Cristina Jung <cristijung@gmail.com>
2023-04-28 10:26:39 -0300

    ajustes de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'agenda', component: AgendaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
